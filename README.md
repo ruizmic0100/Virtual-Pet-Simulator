1193bec678048f253c79cf3294a1d01b503666cd
michael <ruizmic0100@hotmail.com>
2022-01-06 20:53:45 -0800

    Added pyzero gui hello world.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pico_project_c
 Raspberry Pi Pico Project written in C++/C
+
+# Build Process
+    mkdir build
+    cd build
+    export PICO_SDK_PATH=../../pico-sdk
+    cmake ..
+    make
--- test.c
+++ test.c
@@ -11,10 +11,14 @@ int main() {
     bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
     stdio_init_all();
-    
+
+
+
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (1) {
+
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         gpio_put(LED_PIN, 1);
